48b1748e5238d144503a997bd66ad960f220daa2
sonms <smsms5676@naver.com>
2023-05-16 22:27:35 +0900

    feature/AND/댓글 데이터 세팅, 댓글 읽기 리사이클러뷰 세팅

--- app/src/main/java/com/example/mio/NoticeBoardReadActivity.kt
+++ app/src/main/java/com/example/mio/NoticeBoardReadActivity.kt
@@ -2,12 +2,31 @@ package com.example.mio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.animation.OvershootInterpolator
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mio.Adapter.NoticeBoardAdapter
+import com.example.mio.Adapter.NoticeBoardReadAdapter
+import com.example.mio.Model.CommentData
 import com.example.mio.Model.PostData
 import com.example.mio.databinding.ActivityNoticeBoardBinding
 import com.example.mio.databinding.ActivityNoticeBoardReadBinding
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class NoticeBoardReadActivity : AppCompatActivity() {
     private lateinit var nbrBinding : ActivityNoticeBoardReadBinding
+    private var manager : LinearLayoutManager = LinearLayoutManager(this)
+    //private var noticeBoardAdapter : NoticeBoardAdapter? = null
+    private var noticeBoardReadAdapter : NoticeBoardReadAdapter? = null
+
+    //댓글 저장 전체 데이터
+    private var commentAllData = mutableListOf<CommentData?>()
+
+    //클릭한 포스트(게시글)의 데이터 임시저장
     private var temp : PostData? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,15 +34,72 @@ class NoticeBoardReadActivity : AppCompatActivity() {
         nbrBinding = ActivityNoticeBoardReadBinding.inflate(layoutInflater)
         setContentView(nbrBinding.root)
 
+        initRecyclerView()
+        sendComment()
+
         val type = intent.getStringExtra("type")
 
         if (type.equals("READ")) {
             temp = intent.getSerializableExtra("postItem") as PostData?
             nbrBinding.readContentText.text = temp!!.postContent
+            nbrBinding.readAccountId.text = temp!!.accountID
         }
 
 
 
 
+
+    }
+
+    private fun initRecyclerView() {
+        noticeBoardReadAdapter = NoticeBoardReadAdapter()
+        noticeBoardReadAdapter!!.commentItemData = commentAllData
+        nbrBinding.commentRV.adapter = noticeBoardReadAdapter
+        //레이아웃 뒤집기 안씀
+        //manager.reverseLayout = true
+        //manager.stackFromEnd = true
+        nbrBinding.commentRV.setHasFixedSize(true)
+        nbrBinding.commentRV.layoutManager = manager
+
+        nbrBinding.commentRV.itemAnimator =  SlideInUpAnimator(OvershootInterpolator(1f))
+        nbrBinding.commentRV.itemAnimator?.apply {
+            addDuration = 1000
+            removeDuration = 100
+            moveDuration = 1000
+            changeDuration = 100
+        }
+    }
+
+    private fun sendComment() {
+        var et = ""
+        nbrBinding.messageET.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                et = nbrBinding.messageET.text.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        })
+        nbrBinding.messageSendIV.setOnClickListener {
+            if (et.isEmpty()) {
+                Toast.makeText(this@NoticeBoardReadActivity, "내용을 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                //나중에 여기서 데이터 변경하기 현재 로그인된 계정정보로 Todo
+                commentAllData.add(CommentData("2020202", et, 0))
+                noticeBoardReadAdapter!!.notifyDataSetChanged()
+                nbrBinding.messageET.text.clear()
+            }
+        }
+        /*nbrBinding.messageSendIV.setOnClickListener {
+            if (et.isEmpty()) {
+                Toast.makeText(this, "내용을 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                //나중에 여기서 데이터 변경하기 현재 로그인된 계정정보로 Todo
+                commentAllData.add(CommentData("2020202", et, 0))
+                noticeBoardReadAdapter!!.notifyDataSetChanged()
+            }
+        }*/
     }
 }
